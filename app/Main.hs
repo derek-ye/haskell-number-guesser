f9abbbbd3e3299fce707c8d1f495c5d7561c0c61
Derek <derek@mercury.com>
2022-08-25 10:11:52 -0700

    Add ability to take guess input

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,27 @@
 module Main (main) where
 
-getGuess :: IO ()
-getGuess = do
+getGuess :: (Int -> Bool) -> IO ()
+getGuess check = do
     guess <- getLine
-    case isCorrectGuess $ read guess of
+    case check $ read guess of
         True -> putStrLn "You guessed the right number!"
         False -> do
             putStrLn "Wrong number, try again"
-            getGuess
+            getGuess check
 
-isCorrectGuess :: Int -> Bool
-isCorrectGuess x = x == 5
+getCorrectAns :: IO Int
+getCorrectAns = do
+    correctAns <- readLn :: IO Int
+    pure correctAns
+
+isCorrectGuess :: Int -> Int -> Bool
+isCorrectGuess ans guess = ans == guess
 
 main :: IO ()
 main = do
+    putStrLn "What is the secret number?"
+    ans <- getCorrectAns
+    let check = isCorrectGuess ans
     putStrLn "Make a guess"
-    getGuess
+    getGuess check
 
